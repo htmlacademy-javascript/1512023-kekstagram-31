6eab2d1bbb57901bb39185f5eb180aff5a01dcc3
Ludmila Zabrovskaya <forex79637785441@yandex.ru>
2024-02-20 20:31:50 +0300

    рефакторинг задач 2 и 3 (после лайва)

--- js/functions.js
+++ js/functions.js
@@ -4,34 +4,28 @@ const compareLength = (incomingString = '', maxLength = 1) => incomingString.len
 
 /* Функция для проверки, является ли строка палиндромом*/
 
-function isPalindrome (testableString){
-  const normalisedString = testableString.replaceAll(' ', '').toLowerCase();
+function isPalindrome (testableString = ''){
+  testableString = testableString.replaceAll(' ', '').toLowerCase();
   let invertedString = '';
-  for (let i = normalisedString.length - 1; i >= 0; i--){
-    invertedString += normalisedString[i];
+  for (let i = testableString.length - 1; i >= 0; i--){
+    invertedString += testableString[i];
   }
-  return (invertedString === normalisedString);
+  return (invertedString === testableString);
 }
 
 /*Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
  и возвращает их в виде целого положительного числа.
  Если в строке нет ни одной цифры, функция должна вернуть NaN
  */
-
-function extractNumbers (charSet){
-  let stringSet = '';
+function extractNumbers (someInput){
   let tempResult = '';
   let result = 0;
 
-  if (isNaN(charSet)){
-    stringSet = charSet;
-  }else {
-    stringSet = charSet.toString();
-  }
+  someInput = someInput.toString();
 
-  for (let i = 0; i <= stringSet.length - 1; i++){
-    if(!isNaN(parseInt(stringSet[i],10))){
-      tempResult += parseInt(stringSet[i],10);
+  for (let i = 0; i <= someInput.length - 1; i++){
+    if(!isNaN(parseInt(someInput[i],10))){
+      tempResult += parseInt(someInput[i],10);
     }
   }
 
