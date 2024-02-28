b87e9e10f9b34d1776874cac072ae85c2a1707ce
Ludmila Zabrovskaya <forex79637785441@yandex.ru>
2024-02-28 10:45:39 +0300

    сделано ДЗ Нужно больше информации (модуль 4)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const PHOTO_ID_MIN = 1;
 const PHOTO_ID_MAX = 25;
 const LIKES_MIN = 15;
 const LIKES_MAX = 200;
+const COMMENTS_COUNT_MIN = 0;
+const COMMENTS_COUNT_MAX = 30;
+const DESCRIPTIONS_COUNT = 25;
 
 const MESSAGE_SOURCE = [
   'Всё отлично!',
@@ -18,17 +21,14 @@ function getRandomInteger (min, max) {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
-
   return Math.floor(result);
 }
 
 function createRandomIdFromRangeGenerator (min, max) {
   const previousValues = [];
-
   return function () {
     let currentValue = getRandomInteger(min, max);
     if (previousValues.length >= (max - min + 1)) {
-      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
       return null;
     }
     while (previousValues.includes(currentValue)) {
@@ -40,12 +40,9 @@ function createRandomIdFromRangeGenerator (min, max) {
 }
 
 function createIdGenerator (min, max) {
-
   let lastGeneratedId = min;
-
   return function () {
     if (lastGeneratedId > max){
-      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
       return null;
     }
     lastGeneratedId ++;
@@ -53,52 +50,32 @@ function createIdGenerator (min, max) {
   };
 }
 
-const getRandomArrayElement = (elements) =>
-elements[getRandomInteger(0, elements.length - 1)];
+function getComments (elements) {
+  const randomIndex = getRandomInteger(COMMENTS_COUNT_MIN, COMMENTS_COUNT_MAX);
+  const commentsArray = [];
+  for (let i = 1; i <= randomIndex;i++){
+    commentsArray.push(elements[getRandomInteger(0, elements.length - 1)]);
+  }
+  return commentsArray;
+}
 
-console.log(getRandomArrayElement(MESSAGE_SOURCE));
 
 const getIdPhoto = createIdGenerator(PHOTO_ID_MIN,PHOTO_ID_MAX);
 const getIdUrl = createIdGenerator(PHOTO_ID_MIN,PHOTO_ID_MAX);
 const getIdForDescription = createIdGenerator(PHOTO_ID_MIN,PHOTO_ID_MAX);
 const getLikesCount = createRandomIdFromRangeGenerator (LIKES_MIN, LIKES_MAX);
-const getIndexForComments = createRandomIdFromRangeGenerator (LIKES_MIN, LIKES_MAX);
-
-const getComments = getRandomArrayElement(MESSAGE_SOURCE);
-
-
-/*Структура каждого объекта должна быть следующей:
-
-id, число — идентификатор опубликованной фотографии. Это число от 1 до 25.
-Идентификаторы не должны повторяться.
-
-url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25.
-Адреса картинок не должны повторяться.
-
-description, строка — описание фотографии. Описание придумайте самостоятельно.
-
-likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
-
-comments, массив объектов — список комментариев, оставленных другими пользователями
- к этой фотографии.
- Количество комментариев к каждой фотографии — случайное число от 0 до 30.
- Все комментарии генерируются случайным образом.  */
 
 const createPhotoDescription = () => ({
   id: getIdPhoto(createIdGenerator()),
-  url: 'photos/' + getIdUrl(createIdGenerator()) +'.jpg',
-  description: 'The description of the photo Nr. ' + getIdForDescription(createIdGenerator()),
+  url: `photos/${getIdUrl(createIdGenerator())}.jpg`,
+  description: `The description of the photo Nr.${getIdForDescription(createIdGenerator())}.`,
   likes: getLikesCount(createRandomIdFromRangeGenerator()),
-  comments: getRandomArrayElement(MESSAGE_SOURCE),
+  comments: getComments(MESSAGE_SOURCE),
   //coatColor: getRandomArrayElement(COAT_COLORS),
   //eyesColor: getRandomArrayElement(EYES_COLORS),
 });
 
-const test = createPhotoDescription();
-const test2 = createPhotoDescription();
-const test3 = createPhotoDescription();
-const test4 = createPhotoDescription();
-console.log(test);
-console.log(test2);
-console.log(test3);
-console.log(test4);
+
+const photoDescriptions = Array.from({length: DESCRIPTIONS_COUNT}, createPhotoDescription);
+
+photoDescriptions();
