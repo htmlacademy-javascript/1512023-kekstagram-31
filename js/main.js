1fe4fb0a8ea33a17900cf85f92f7325e4d8da3e2
Ludmila Zabrovskaya <forex79637785441@yandex.ru>
2024-03-01 21:35:51 +0300

    исправила ошибку (добавила массив комментариев)

--- js/main.js
+++ js/main.js
@@ -5,16 +5,27 @@ const LIKES_MAX = 200;
 const COMMENTS_COUNT_MIN = 0;
 const COMMENTS_COUNT_MAX = 30;
 const DESCRIPTIONS_COUNT = 25;
+const AVATAR_ID_MIN = 1;
+const AVATAR_ID_MAX = 6;
 
 const MESSAGE_SOURCE = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
-  'В конце концов это просто непрофессионально.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают.',
-  'Как можно было поймать такой неудачный момент?!'
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Вася',
+  'Коля',
+  'Петя',
+  'Таня',
+  'Ира',
+  'Дина',
+  'Ваня',
+  'Маша'
 ];
 
 function getRandomInteger (min, max) {
@@ -50,31 +61,28 @@ function createIdGenerator (min, max) {
   };
 }
 
-function getComments (elements) {
-  const randomIndex = getRandomInteger(COMMENTS_COUNT_MIN, COMMENTS_COUNT_MAX);
-  const commentsArray = [];
-  for (let i = 1; i <= randomIndex;i++){
-    commentsArray.push(elements[getRandomInteger(0, elements.length - 1)]);
-  }
-  return commentsArray;
-}
-
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const getIdPhoto = createIdGenerator(PHOTO_ID_MIN,PHOTO_ID_MAX);
 const getIdUrl = createIdGenerator(PHOTO_ID_MIN,PHOTO_ID_MAX);
 const getIdForDescription = createIdGenerator(PHOTO_ID_MIN,PHOTO_ID_MAX);
 const getLikesCount = createRandomIdFromRangeGenerator (LIKES_MIN, LIKES_MAX);
+const getCommentsID = createRandomIdFromRangeGenerator(COMMENTS_COUNT_MIN, COMMENTS_COUNT_MAX);
+
+const createComments = () => ({
+  id: getCommentsID(createRandomIdFromRangeGenerator()),
+  avatar: `img/avatar-${getRandomInteger(AVATAR_ID_MIN,AVATAR_ID_MAX)}.svg`,
+  message: getRandomArrayElement(MESSAGE_SOURCE),
+  name: getRandomArrayElement(NAMES),
+});
 
 const createPhotoDescription = () => ({
   id: getIdPhoto(createIdGenerator()),
   url: `photos/${getIdUrl(createIdGenerator())}.jpg`,
   description: `The description of the photo Nr.${getIdForDescription(createIdGenerator())}.`,
   likes: getLikesCount(createRandomIdFromRangeGenerator()),
-  comments: getComments(MESSAGE_SOURCE),
-  //coatColor: getRandomArrayElement(COAT_COLORS),
-  //eyesColor: getRandomArrayElement(EYES_COLORS),
+  comments: Array.from({length:getRandomInteger(COMMENTS_COUNT_MIN,COMMENTS_COUNT_MAX)}, createComments),
 });
-
 
 const photoDescriptions = Array.from({length: DESCRIPTIONS_COUNT}, createPhotoDescription);
 
